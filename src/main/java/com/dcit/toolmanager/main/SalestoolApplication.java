59a4b6f76192a34fc1c356dc062a50c08203d2ed
Younes Elmestari <elmestari@delta-it-solutions.com>
2019-10-25 16:54:34 +0200

    JUMPIN-T001 Architekturaufbau geaendert

---
+++ src/main/java/com/dcit/toolmanager/main/SalestoolApplication.java
@@ -0,0 +1,12 @@
+package com.dcit.toolmanager.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestoolApplication.class, args);
+	}
+}
