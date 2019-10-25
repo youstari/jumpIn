c598fb9eedb14f4d674c5785f3642fc255695010
Younes Elmestari <elmestari@delta-it-solutions.com>
2019-10-25 16:09:24 +0200

    JMPIN-T001 Erstellung des Core-Projektes

---
+++ src/main/java/com/deltasolutions/salestool/SalestoolApplication.java
@@ -0,0 +1,12 @@
+package com.deltasolutions.salestool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestoolApplication.class, args);
+	}
+}
